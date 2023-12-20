942a036820c6dbc25ca85557d99830b9492a5b4b
parallel <parallelkim12@gmail.com>
2023-12-21 03:06:34 +0900

    Feat: Update LpPlayer

--- src/components/models/LpPlayer.tsx
+++ src/components/models/LpPlayer.tsx
@@ -3,9 +3,9 @@ import { Gltf } from "@react-three/drei";
 export const LpPlayer = () => {
     return (
         <Gltf
-            position={[20, 0, 0]}
-            scale={[1 / 60, 1 / 60, 1 / 60]}
-            rotation={[Math.PI / 2, 0, 0]}
+            position={[21, 12, 0]}
+            scale={[1 / 30, 1 / 30, 1 / 30]}
+            rotation={[-Math.PI / 2, 0, -Math.PI / 2]}
             src="/lpPlayer.glb"
             receiveShadow
             castShadow
