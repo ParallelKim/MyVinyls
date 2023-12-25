26397b742f866db2eed404a6b01be25cad3fe941
Parallel <parallelkim12@gamil.com>
2023-12-26 02:34:16 +0900

    Feat: Add artist object

---
+++ src/constants/jungwoo.ts
@@ -0,0 +1,43 @@
+import { Album } from "../types/Album";
+
+export const JUNGWOO: { albums: Album[] } = {
+    albums: [
+        {
+            title: "여섯 번째 토요일",
+            artist: "정우",
+            list: [
+                "꽃이 진다면",
+                "나에게서 당신에게",
+                "이름",
+                "일기장",
+                "숙취",
+                "뭐든 될 수 있을 거야",
+                "숙희에게 (feat. 김사월)",
+                "공중댄스",
+                "여섯 번째 토요일",
+                "외로움",
+                "자장가",
+            ],
+            cover: "https://image.aladin.co.kr/img/img_content/C672834975_p.jpg",
+            url: "https://www.youtube.com/watch?v=RR3UGT_Y3xY&list=OLAK5uy_kwUBAtUZb46mNA16pZV0qTh5Yqk6X5byc",
+        },
+        {
+            title: "여섯 번째 토요일",
+            artist: "정우",
+            list: [
+                "Toss and turn",
+                "들불",
+                "낡은 괴담",
+                "클라우드 쿠쿠 랜드",
+                "JUVENILE",
+                "충동 1분",
+                "Strangers",
+                "Gust / Interlude",
+                "허물",
+                "Crack",
+            ],
+            cover: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmusic.bugs.co.kr%2Ftrack%2F33011552%3Fwl_ref%3Dlist_tr_08_chart&psig=AOvVaw2eZF7zqMS2kUZ3Nhm_bgsP&ust=1703609906603000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIC2usyHq4MDFQAAAAAdAAAAABAD",
+            url: "https://www.youtube.com/watch?v=vZVQ3loVjs0&list=OLAK5uy_nhXRJcv8WxjwjfK6Ul7PRMpSog-01_YQ8",
+        },
+    ],
+};
